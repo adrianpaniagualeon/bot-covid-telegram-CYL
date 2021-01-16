3e3af051e7173ec43c8786c808f79c263d5a781e
Adrián <github@adrianpaniagua.es>
2021-01-16 12:29:08 +0100

    Actualizado URL de consulta de datos

--- bot.py
+++ bot.py
@@ -38,127 +38,66 @@ def confirmacion(message):
 	mensaje = message.text
 	respuesta2 = types.ForceReply(selective=False)
 	if mensaje == "SI":
-		try:
-			yesterday = datetime.now() - timedelta(1)
-			yesterday_1 = str(datetime.strftime(yesterday, '%Y-%m-%d'))
-			yesterday_2 = datetime.strftime(yesterday, '%d-%m-%Y')
-			url = 'https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=indicadores-de-riesgo-covid-19-por-municipios&q=&facet=fecha&facet=municipio&facet=provincia&facet=ia14_boe_valoracion&facet=ia7_boe_valoracion&facet=ia14_boe_65mas_valoracion&facet=ia7_boe_65mas_valoracion&facet=positividad_valoracion&facet=porc_trazabilidad_valoracion&facet=casos_ultima_semana&refine.municipio='+PUEBLO+'&refine.fecha='+yesterday_1
+
+		url = 'https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=indicadores-de-riesgo-covid-19-por-municipios&q=&rows=10&sort=fecha&facet=fecha&facet=municipio&facet=provincia&facet=ia14_boe_valoracion&facet=ia7_boe_valoracion&facet=ia14_boe_65mas_valoracion&facet=ia7_boe_65mas_valoracion&facet=positividad_valoracion&facet=porc_trazabilidad_valoracion&refine.municipio=Sahag%C3%BAn'
 
 
-			respuesta = requests.get(url)
-			open('respuesta.json', 'wb').write(respuesta.content)
-			f = open('respuesta.json')
-			json_file = json.load(f)
-			json_str = json.dumps(json_file)
-			resp = json.loads(json_str)
+		respuesta = requests.get(url)
+		open('respuesta.json', 'wb').write(respuesta.content)
+		f = open('respuesta.json')
+		json_file = json.load(f)
+		json_str = json.dumps(json_file)
+		resp = json.loads(json_str)
+
+		fecha = resp['records'][0]['fields']['fecha']
+
+		ia14 = 	resp['records'][0]['fields']['ia14_boe']
+		ia14_valoracion = 	resp['records'][0]['fields']['ia14_boe_65mas_valoracion']	
+		ia7 =resp['records'][0]['fields']['ia7_boe']
+		ia7_valoracion = resp['records'][0]['fields']['ia7_boe_65mas_valoracion']
+		casos_7d = resp['records'][0]['fields']['casos_ultima_semana']
+		positividad = resp['records'][0]['fields']['positividad']
+		positividad_valoracion = resp['records'][0]['fields']['positividad_valoracion']
+		trazabilidad = resp['records'][0]['fields']['porc_trazabilidad']
+		trazabilidad_valoracion = resp['records'][0]['fields']['porc_trazabilidad_valoracion']
 
 
-			ia14 = 	resp['records'][0]['fields']['ia14_boe']
-			ia14_valoracion = 	resp['records'][0]['fields']['ia14_boe_65mas_valoracion']	
-			ia7 =resp['records'][0]['fields']['ia7_boe']
-			ia7_valoracion = resp['records'][0]['fields']['ia7_boe_65mas_valoracion']
-			casos_7d = resp['records'][0]['fields']['casos_ultima_semana']
-			positividad = resp['records'][0]['fields']['positividad']
-			positividad_valoracion = resp['records'][0]['fields']['positividad_valoracion']
-			trazabilidad = resp['records'][0]['fields']['porc_trazabilidad']
-			trazabilidad_valoracion = resp['records'][0]['fields']['porc_trazabilidad_valoracion']
-
-
-			mensaje_datos = "Datos del Municipio de Sahagún\n📅 Fecha de Datos: "+str(yesterday_2)+"\n\n Incidencia Acumulada (14 dias)\n👉 "+str(ia14)+" ("+ia14_valoracion+")\n\n Incidencia Acumulada (7 dias)\n"+"👉 "+str(ia7)+" ("+ia7_valoracion+")\n\n Positividad: "+"\n👉 "+str(positividad)+" ("+positividad_valoracion+")\n\n Casos última semana: "+"\n👉 "+str(casos_7d)+"\n\n Trazabilidad : \n👉 "+str(trazabilidad)+"% ("+trazabilidad_valoracion+")"
-		except:
-			beforeyesterday = datetime.now() - timedelta(2)
-			beforeyesterday_1 = str(datetime.strftime(beforeyesterday, '%Y-%m-%d'))
-			beforeyesterday_2 = datetime.strftime(beforeyesterday, '%d-%m-%Y')
-			url = 'https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=indicadores-de-riesgo-covid-19-por-municipios&q=&facet=fecha&facet=municipio&facet=provincia&facet=ia14_boe_valoracion&facet=ia7_boe_valoracion&facet=ia14_boe_65mas_valoracion&facet=ia7_boe_65mas_valoracion&facet=positividad_valoracion&facet=porc_trazabilidad_valoracion&facet=casos_ultima_semana&refine.municipio='+PUEBLO+'&refine.fecha='+beforeyesterday_1
-
-
-			respuesta = requests.get(url)
-			open('respuesta.json', 'wb').write(respuesta.content)
-			f = open('respuesta.json')
-			json_file = json.load(f)
-			json_str = json.dumps(json_file)
-			resp = json.loads(json_str)
-
-
-			ia14 = 	resp['records'][0]['fields']['ia14_boe']
-			ia14_valoracion = 	resp['records'][0]['fields']['ia14_boe_65mas_valoracion']	
-			ia7 =resp['records'][0]['fields']['ia7_boe']
-			ia7_valoracion = resp['records'][0]['fields']['ia7_boe_65mas_valoracion']
-			casos_7d = resp['records'][0]['fields']['casos_ultima_semana']
-			positividad = resp['records'][0]['fields']['positividad']
-			positividad_valoracion = resp['records'][0]['fields']['positividad_valoracion']
-			trazabilidad = resp['records'][0]['fields']['porc_trazabilidad']
-			trazabilidad_valoracion = resp['records'][0]['fields']['porc_trazabilidad_valoracion']
-
-
-			mensaje_datos = "Datos del Municipio de Sahagún\n📅 Fecha de Datos: "+str(beforeyesterday_2)+"\n\n Incidencia Acumulada (14 dias)\n👉 "+str(ia14)+" ("+ia14_valoracion+")\n\n Incidencia Acumulada (7 dias)\n"+"👉 "+str(ia7)+" ("+ia7_valoracion+")\n\n Positividad: "+"\n👉 "+str(positividad)+" ("+positividad_valoracion+")\n\n Casos última semana: "+"\n👉 "+str(casos_7d)+"\n\n Trazabilidad : \n👉 "+str(trazabilidad)+"% ("+trazabilidad_valoracion+")"
-
-		bot.send_message(GROUP_ID, mensaje_datos)
+		mensaje_datos = "Datos del Municipio de Sahagún\n📅 Fecha de Datos: "+str(fecha)+"\n\n Incidencia Acumulada (14 dias)\n👉 "+str(ia14)+" ("+ia14_valoracion+")\n\n Incidencia Acumulada (7 dias)\n"+"👉 "+str(ia7)+" ("+ia7_valoracion+")\n\n Positividad: "+"\n👉 "+str(positividad)+" ("+positividad_valoracion+")\n\n Casos última semana: "+"\n👉 "+str(casos_7d)+"\n\n Trazabilidad : \n👉 "+str(trazabilidad)+"% ("+trazabilidad_valoracion+")"
+		
 	else:
 		bot.send_message(ADMIN_ID, "Los Datos no han sido enviados")
 
 		
 @bot.message_handler(commands={"datos"})
 def datos(message):
-	try:
-		mensaje = message.text
-		cid = message.chat.id
+	mensaje = message.text
+	cid = message.chat.id
 
-		yesterday = datetime.now() - timedelta(1)
-		yesterday_1 = str(datetime.strftime(yesterday, '%Y-%m-%d'))
-		yesterday_2 = datetime.strftime(yesterday, '%d-%m-%Y')
-		url = 'https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=indicadores-de-riesgo-covid-19-por-municipios&q=&facet=fecha&facet=municipio&facet=provincia&facet=ia14_boe_valoracion&facet=ia7_boe_valoracion&facet=ia14_boe_65mas_valoracion&facet=ia7_boe_65mas_valoracion&facet=positividad_valoracion&facet=porc_trazabilidad_valoracion&facet=casos_ultima_semana&refine.municipio=Sahag%C3%BAn&refine.fecha='+yesterday_1
+	yesterday = datetime.now() - timedelta(1)
+	yesterday_1 = str(datetime.strftime(yesterday, '%Y-%m-%d'))
+	fecha = datetime.strftime(yesterday, '%d-%m-%Y')
+	url = 'https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=indicadores-de-riesgo-covid-19-por-municipios&q=&rows=10&sort=fecha&facet=fecha&facet=municipio&facet=provincia&facet=ia14_boe_valoracion&facet=ia7_boe_valoracion&facet=ia14_boe_65mas_valoracion&facet=ia7_boe_65mas_valoracion&facet=positividad_valoracion&facet=porc_trazabilidad_valoracion&refine.municipio=Sahag%C3%BAn'
 
-		respuesta = requests.get(url)
-		open('respuesta.json', 'wb').write(respuesta.content)
-		f = open('respuesta.json')
-		json_file = json.load(f)
-		json_str = json.dumps(json_file)
-		resp = json.loads(json_str)
-
-
-		ia14 = 	resp['records'][0]['fields']['ia14_boe']
-		ia14_valoracion = 	resp['records'][0]['fields']['ia14_boe_65mas_valoracion']	
-		ia7 =resp['records'][0]['fields']['ia7_boe']
-		ia7_valoracion = resp['records'][0]['fields']['ia7_boe_65mas_valoracion']
-		casos_7d = resp['records'][0]['fields']['casos_ultima_semana']
-		positividad = resp['records'][0]['fields']['positividad']
-		positividad_valoracion = resp['records'][0]['fields']['positividad_valoracion']
-		trazabilidad = resp['records'][0]['fields']['porc_trazabilidad']
-		trazabilidad_valoracion = resp['records'][0]['fields']['porc_trazabilidad_valoracion']
+	respuesta = requests.get(url)
+	open('respuesta.json', 'wb').write(respuesta.content)
+	f = open('respuesta.json')
+	json_file = json.load(f)
+	json_str = json.dumps(json_file)
+	resp = json.loads(json_str)
 
 
-		mensaje_datos = "Datos del Municipio de Sahagún\n📅 Fecha de Datos: "+str(yesterday_2)+"\n\n Incidencia Acumulada (14 dias)\n👉 "+str(ia14)+" ("+ia14_valoracion+")\n\n Incidencia Acumulada (7 dias)\n"+"👉 "+str(ia7)+" ("+ia7_valoracion+")\n\n Positividad: "+"\n👉 "+str(positividad)+" ("+positividad_valoracion+")\n\n Casos última semana: "+"\n👉 "+str(casos_7d)+"\n\n Trazabilidad : \n👉 "+str(trazabilidad)+"% ("+trazabilidad_valoracion+")"
-	except:
-		mensaje = message.text
-		cid = message.chat.id
-
-		beforeyesterday = datetime.now() - timedelta(2)
-		beforeyesterday_1 = str(datetime.strftime(beforeyesterday, '%Y-%m-%d'))
-		beforeyesterday_2 = datetime.strftime(beforeyesterday, '%d-%m-%Y')
-		
-		url = 'https://analisis.datosabiertos.jcyl.es/api/records/1.0/search/?dataset=indicadores-de-riesgo-covid-19-por-municipios&q=&facet=fecha&facet=municipio&facet=provincia&facet=ia14_boe_valoracion&facet=ia7_boe_valoracion&facet=ia14_boe_65mas_valoracion&facet=ia7_boe_65mas_valoracion&facet=positividad_valoracion&facet=porc_trazabilidad_valoracion&facet=casos_ultima_semana&refine.municipio=Sahag%C3%BAn&refine.fecha='+beforeyesterday_1
-
-		respuesta = requests.get(url)
-		open('respuesta.json', 'wb').write(respuesta.content)
-		f = open('respuesta.json')
-		json_file = json.load(f)
-		json_str = json.dumps(json_file)
-		resp = json.loads(json_str)
+	ia14 = 	resp['records'][0]['fields']['ia14_boe']
+	ia14_valoracion = 	resp['records'][0]['fields']['ia14_boe_65mas_valoracion']	
+	ia7 =resp['records'][0]['fields']['ia7_boe']
+	ia7_valoracion = resp['records'][0]['fields']['ia7_boe_65mas_valoracion']
+	casos_7d = resp['records'][0]['fields']['casos_ultima_semana']
+	positividad = resp['records'][0]['fields']['positividad']
+	positividad_valoracion = resp['records'][0]['fields']['positividad_valoracion']
+	trazabilidad = resp['records'][0]['fields']['porc_trazabilidad']
+	trazabilidad_valoracion = resp['records'][0]['fields']['porc_trazabilidad_valoracion']
 
 
-		ia14 = 	resp['records'][0]['fields']['ia14_boe']
-		ia14_valoracion = 	resp['records'][0]['fields']['ia14_boe_65mas_valoracion']	
-		ia7 =resp['records'][0]['fields']['ia7_boe']
-		ia7_valoracion = resp['records'][0]['fields']['ia7_boe_65mas_valoracion']
-		casos_7d = resp['records'][0]['fields']['casos_ultima_semana']
-		positividad = resp['records'][0]['fields']['positividad']
-		positividad_valoracion = resp['records'][0]['fields']['positividad_valoracion']
-		trazabilidad = resp['records'][0]['fields']['porc_trazabilidad']
-		trazabilidad_valoracion = resp['records'][0]['fields']['porc_trazabilidad_valoracion']
-
-
-		mensaje_datos = "Datos del Municipio de Sahagún\n📅 Fecha de Datos: "+str(beforeyesterday_2)+"\n\n Incidencia Acumulada (14 dias)\n👉 "+str(ia14)+" ("+ia14_valoracion+")\n\n Incidencia Acumulada (7 dias)\n"+"👉 "+str(ia7)+" ("+ia7_valoracion+")\n\n Positividad: "+"\n👉 "+str(positividad)+" ("+positividad_valoracion+")\n\n Casos última semana: "+"\n👉 "+str(casos_7d)+"\n\n Trazabilidad : \n👉 "+str(trazabilidad)+"% ("+trazabilidad_valoracion+")"
+	mensaje_datos = "Datos del Municipio de Sahagún\n📅 Fecha de Datos: "+str(fecha)+"\n\n Incidencia Acumulada (14 dias)\n👉 "+str(ia14)+" ("+ia14_valoracion+")\n\n Incidencia Acumulada (7 dias)\n"+"👉 "+str(ia7)+" ("+ia7_valoracion+")\n\n Positividad: "+"\n👉 "+str(positividad)+" ("+positividad_valoracion+")\n\n Casos última semana: "+"\n👉 "+str(casos_7d)+"\n\n Trazabilidad : \n👉 "+str(trazabilidad)+"% ("+trazabilidad_valoracion+")"
 
 	bot.send_message(cid, mensaje_datos)
 
